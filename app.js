22d319742b1a7f8d474dcd46d9523c732ac798a5
Gagandeep <singh.gagandeep0034@gmail.com>
2019-03-04 06:48:42 -0600

    Added event listeners to all buttons

--- app.js
+++ app.js
@@ -1,23 +1,16 @@
-
-
-var key = function(arg){
-	return eval(`document.querySelector(.${arg})`);
-}
-
-var one = key('one');
-var two = key('two');
-var three = key('three');
-var four = key('four');
-var five = key('five');
-var six = key('six');
-var seven = key('seven');
-var eight = key('eight');
-var nine = key('nine');
-var zero = ('zero');
-var plus = ('plus');
-var minus = ('minus');
-var multi = ('multi');
-var modulo = ('modulo');
-var divide = ('divide');
-var equals = ('equals');
-var apple = document.querySelector('.apple');
+var one = document.querySelector(".one");
+var two = document.querySelector(".two");
+var three = document.querySelector(".three");
+var four = document.querySelector(".four");
+var five = document.querySelector(".five");
+var six = document.querySelector(".six");
+var seven = document.querySelector(".seven");
+var eight = document.querySelector(".eight");
+var nine = document.querySelector(".nine");
+var zero = document.querySelector(".zero");
+var plus = document.querySelector(".plus");
+var minus = document.querySelector(".minus");
+var multi = document.querySelector(".multi");
+var modulo = document.querySelector(".modulo");
+var divide = document.querySelector(".divide");
+var equals = document.querySelector(".equals");
