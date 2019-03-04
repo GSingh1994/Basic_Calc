f6f1d4d216810fc7281c6056bbb8924220547c16
Gagandeep <singh.gagandeep0034@gmail.com>
2019-03-04 06:45:32 -0600

    Tried to select buttons with a function

---
+++ app.js
@@ -0,0 +1,23 @@
+
+
+var key = function(arg){
+	return eval(`document.querySelector(.${arg})`);
+}
+
+var one = key('one');
+var two = key('two');
+var three = key('three');
+var four = key('four');
+var five = key('five');
+var six = key('six');
+var seven = key('seven');
+var eight = key('eight');
+var nine = key('nine');
+var zero = ('zero');
+var plus = ('plus');
+var minus = ('minus');
+var multi = ('multi');
+var modulo = ('modulo');
+var divide = ('divide');
+var equals = ('equals');
+var apple = document.querySelector('.apple');
