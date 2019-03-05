5dd7650c15c1c0ef350bb23b1ea507dccce8299d
Gagandeep <singh.gagandeep0034@gmail.com>
2019-03-04 19:23:44 -0600

    Added event listeners

---
+++ app.js
@@ -0,0 +1,18 @@
+var one = document.querySelector('.one');
+var two = document.querySelector('.two');
+var three = document.querySelector('.three');
+var four = document.querySelector('.four');
+var five = document.querySelector('.five');
+var six = document.querySelector('.six');
+var seven = document.querySelector('.seven');
+var eight = document.querySelector('.eight');
+var nine = document.querySelector('.nine');
+var zero = document.querySelector('.zero');
+
+var multi = document.querySelector('.multi');
+var plus = document.querySelector('.plus');
+var minus = document.querySelector('.minus');
+var equals = document.querySelector('.equals');
+var divide = document.querySelector('.divide');
+var modulo = document.querySelector('.modulo');
+
