b04c9e8b82e3f9c529e28170ad4f57c43300e23c
Gagandeep <singh.gagandeep0034@gmail.com>
2019-03-04 20:56:08 -0600

    Event listeners added

--- app.js
+++ app.js
@@ -14,5 +14,74 @@ var plus = document.querySelector('.plus');
 var minus = document.querySelector('.minus');
 var equals = document.querySelector('.equals');
 var divide = document.querySelector('.divide');
-var modulo = document.querySelector('.modulo');
+var decimal = document.querySelector('.decimal');
+
+var display = document.querySelector('.display');
+
+
+one.addEventListener('click', function(){
+	console.log(1);
+	
+})
+
+two.addEventListener('click', function(){
+	
+})
+
+three.addEventListener('click', function(){
+	
+})
+
+four.addEventListener('click', function(){
+	
+})
+
+five.addEventListener('click', function(){
+	
+})
+
+six.addEventListener('click', function(){
+	
+})
+
+seven.addEventListener('click', function(){
+	
+})
+
+eight.addEventListener('click', function(){
+	
+})
+
+nine.addEventListener('click', function(){
+	
+})
+
+zero.addEventListener('click', function(){
+	
+})
+
+plus.addEventListener('click', function(){
+
+})
+
+
+multi.addEventListener('click', function(){
+
+})
+
+minus.addEventListener('click', function(){
+
+})
+
+equals.addEventListener('click', function(){
+
+})
+
+decimal.addEventListener('click', function(){
+
+})
+
+divide.addEventListener('click', function(){
+
+})
 
